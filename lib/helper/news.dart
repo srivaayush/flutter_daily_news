2f273049eca4d0411fa01c11a98010a153fa0faf
Ayush srivastava <ayush.sriva6@gmail.com>
2022-04-20 07:45:16 +0530

    Flutter Daily News App

---
+++ lib/helper/news.dart
@@ -0,0 +1,37 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import '../models/article.dart';
+
+class News {
+  List<Article> news = [];
+
+  Future<void> getNews() async {
+    String url =
+        "http://newsapi.org/v2/top-headlines?country=in&excludeDomains=stackoverflow.com&sortBy=publishedAt&language=en&apiKey=3b66710c90594d1194ca7412830ca1ac";
+
+    http.Response response = await http.get(Uri.parse(url));
+
+    
+    
+    var jsonData = jsonDecode(response.body);
+  
+    if (jsonData['status'] == "ok") {
+      jsonData["articles"].forEach((element) {
+        if (element['urlToImage'] != null && element['description'] != null) {
+          Article article = Article(
+            title: element['title'],
+            author: element['author'],
+            description: element['description'],
+            urlToImage: element['urlToImage'],
+            publshedAt: DateTime.parse(element['publishedAt']),
+            content: element["content"],
+            articleUrl: element["url"],
+          );
+          news.add(article);
+        }
+      });
+    }
+  }
+}
+
---
+++ lib/helper/news_category.dart
@@ -0,0 +1,33 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:flutter_daily_news/models/article.dart';
+
+class NewsForCategory {
+  List<Article> news = [];
+
+  Future<void> getNewsForCategory(String category) async {
+    String url =
+        "http://newsapi.org/v2/top-headlines?country=in&category=$category&apiKey=3b66710c90594d1194ca7412830ca1ac";
+
+    var response = await http.get(Uri.parse(url));
+
+    var jsonData = jsonDecode(response.body);
+
+    if (jsonData['status'] == "ok") {
+      jsonData["articles"].forEach((element) {
+        if (element['urlToImage'] != null && element['description'] != null) {
+          Article article = Article(
+            title: element['title'],
+            author: element['author'],
+            description: element['description'],
+            urlToImage: element['urlToImage'],
+            publshedAt: DateTime.parse(element['publishedAt']),
+            content: element["content"],
+            articleUrl: element["url"],
+          );
+          news.add(article);
+        }
+      });
+    }
+  }
+}
